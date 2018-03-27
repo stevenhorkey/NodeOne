9f33553a8a6ee1820a0ca4923d0e26eec7d603ed
stevenhorkey <stevenhorkey@gmail.com>
2018-03-26 20:59:21 -0700

    progress

--- liri.js
+++ liri.js
@@ -1,11 +1,13 @@
 require("dotenv").config();
 var fs = require('fs')
 var request = require("request");
+var Spotify = require('node-spotify-api');
+var Twitter = require('twitter');
 
 var keys = require('./key.js')
 
-// var spotify = new Spotify(keys.spotify);
-// var client = new Twitter(keys.twitter);
+var spotify = new Spotify(keys.spotify);
+var client = new Twitter(keys.twitter);
 
 var command = process.argv[2]
 var input = process.argv[3]
