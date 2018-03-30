48745cad9ba010cd8e1fb863e04ab9a825dcad55
stevenhorkey <stevenhorkey@gmail.com>
2018-03-29 18:30:51 -0700

    progress

--- liri.js
+++ liri.js
@@ -10,41 +10,53 @@ var spotify = new Spotify(keys.spotify);
 var client = new Twitter(keys.twitter);
 
 var command = process.argv[2]
-var input = process.argv[3]
+var nodeInput = process.argv[3]
 
 function myTweets(input){
     var params = {screen_name: 'stevedevtech'};
     client.get('statuses/user_timeline', params, function(error, tweets, response) {
         if (error) {
             throw error;
-         } else {
+        } else {
             // console.log(tweets);
             for (var i = 0; i<20; i++){
                 console.log('-----------------------')
                 console.log(tweets[i].text)
                 console.log(tweets[i].created_at)
-                console.log('-----------------------')
-                
+                console.log('-----------------------') 
             }
-         }
-});
-    
+        }
+    });   
 }
 function spotifyThisSong(input){
-    spotify.search({ type: 'track', query: input }, function(err, data) {
-        if (err) {
-          return console.log('Error occurred: ' + err);
-        } else{
-            console.log('----------------------'); 
-            console.log(data.tracks.items[0].artists[0].name); 
-            console.log(data.tracks.items[0].name); 
-            console.log(data.tracks.items[0].preview_url); 
-            console.log(data.tracks.items[0].album.name);
-            console.log('----------------------'); 
-        }
-       
-      });
-
+    if (input!==undefined){
+        spotify.search({ type: 'track', query: input }, function(err, data) {
+            if (err) {
+              return console.log('Error occurred: ' + err);
+            } else{
+                console.log('----------------------'); 
+                console.log(data.tracks.items[0].artists[0].name); 
+                console.log(data.tracks.items[0].name); 
+                console.log(data.tracks.items[0].preview_url); 
+                console.log(data.tracks.items[0].album.name);
+                console.log('----------------------'); 
+            }
+        });
+    } else{
+        spotify.search({ type: 'track', query: 'Zeppelin' }, function(err, data) {
+            if (err) {
+              return console.log('Error occurred: ' + err);
+            } else{
+                console.log('----------------------'); 
+                console.log(data.tracks.items[0].artists[0].name); 
+                console.log(data.tracks.items[0].name); 
+                console.log(data.tracks.items[0].preview_url); 
+                console.log(data.tracks.items[0].album.name);
+                console.log('----------------------'); 
+            }
+        });
+    }
+    
 }
 function movieThis(input){
     if (input){
@@ -79,22 +91,45 @@ function movieThis(input){
             }
         });
     }
-    
-    
 }
 function doWhatItSays(input){
-    
+    var fs = require('fs'),
+    path = require('path')   
 
+    fs.readFile("random.txt", "utf8", function (error, data){
+
+        if (error) {
+          return console.log(error);
+        }
+      
+        console.log(data);
+      
+        var dataArr = data.split(",");
+      
+        console.log(dataArr);
+
+        doThisCommand = dataArr[0];
+        doThisInput = dataArr[1];
+
+        console.log(doThisCommand)
+        console.log(doThisInput)
+
+        conditionals(doThisCommand,doThisInput)
+    });
 }
 
-if (command === 'my-tweets'){
-    myTweets(input)
-} else if (command === 'spotify-this-song'){
-    spotifyThisSong(input)
-} else if (command === 'movie-this'){
-    movieThis(input)
-} else if (command === 'do-what-it-says'){
-    doWhatItSays(input)
-} else {
-    console.log('error')
+function conditionals(command,nodeInput){
+    if (command === 'my-tweets'){
+        myTweets(nodeInput)
+    } else if (command === 'spotify-this-song'){
+        spotifyThisSong(nodeInput)
+    } else if (command === 'movie-this'){
+        movieThis(nodeInput)
+    } else if (command === 'do-what-it-says'){
+        doWhatItSays(nodeInput)
+    } else {
+        console.log('error!!!')
+    }
 }
+
+conditionals(command, nodeInput)
